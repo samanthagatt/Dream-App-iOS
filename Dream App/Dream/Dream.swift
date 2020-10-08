e7b0b6ee6b44e276fb62cffe75b9d3e83c75e38e
Perez <perez@sobergrid.com>
2020-10-07 22:59:40 -0400

    create dream struct

---
+++ Dream App/Dream/Dream.swift
@@ -0,0 +1,17 @@
+//
+//  Dream.swift
+//  Dream App
+//
+//  Created by Perez Willie-Nwobu on 10/7/20.
+//  Copyright © 2020 Samantha Gatt. All rights reserved.
+//
+
+import Foundation
+
+struct Dream {
+    var title : String
+    var description : String
+    var date : String
+    var identifier : String
+    var recording : Data
+}
