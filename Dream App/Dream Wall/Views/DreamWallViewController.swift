80fad50365b13964b1241c3bbe6158bb3ce12b17
Perez <perez@sobergrid.com>
2020-10-13 15:18:14 -0400

    search dreams

--- Dream App/Dream Wall/Views/DreamWallViewController.swift
+++ Dream App/Dream Wall/Views/DreamWallViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 
 
-class DreamWallViewController: UIViewController {
+class DreamWallViewController: UIViewController, UISearchBarDelegate {
     @IBOutlet weak var dreamWallTableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    // Properties
+    var filteredDreams: [Dream] = []
+    var isSearching = false
     
     // Lifecycle
     override func viewDidLoad() {
@@ -30,24 +34,40 @@ class DreamWallViewController: UIViewController {
 extension DreamWallViewController: UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if isSearching {
+            return filteredDreams.count
+        } else {
         return DreamViewModel.shared.dreamArray.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "dreamCell", for: indexPath) as? DreamTableViewCell else { return UITableViewCell() }
-        cell.dream = DreamViewModel.shared.dreamArray[indexPath.row];        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            let dream = DreamViewModel.shared.dreamArray[indexPath.row]
-            DreamViewModel.shared.deleteDream(dream: dream)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            dreamWallTableView.reloadData()
+        if isSearching {
+            cell.dream = filteredDreams[indexPath.row]
+        } else {
+          cell.dream = DreamViewModel.shared.dreamArray[indexPath.row]
         }
+        return cell
     }
 }
 
+// SearchBar
+extension DreamWallViewController {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text == "" {
+               isSearching = false
+               dreamWallTableView.reloadData()
+           } else {
+               isSearching = true
+            filteredDreams = DreamViewModel.shared.dreamArray.filter({$0.title.lowercased().contains(searchBar.text?.lowercased() ?? "")})
+               dreamWallTableView.reloadData()
+           }
+    }
+}
+
+// Private func
 private extension DreamWallViewController {
     func reloadData(){
         DreamViewModel.shared.loadFromPersistence()
@@ -59,6 +79,7 @@ private extension DreamWallViewController {
         searchBar.tintColor = #colorLiteral(red: 0.4254588485, green: 0.3910192549, blue: 0.9995513558, alpha: 1)
         let textFieldInsideSearchBar = searchBar.value(forKey: "searchField") as? UITextField
         textFieldInsideSearchBar?.textColor = .white
+        searchBar.delegate = self
     }
 }
 
