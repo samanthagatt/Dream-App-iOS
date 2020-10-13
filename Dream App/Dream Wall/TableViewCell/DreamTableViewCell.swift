5443310b2aecf51b015add493d99d5aa783c0c64
Perez <perez@sobergrid.com>
2020-10-12 23:59:08 -0400

    add date formatter to dream wall cell

--- Dream App/Dream Wall/TableViewCell/DreamTableViewCell.swift
+++ Dream App/Dream Wall/TableViewCell/DreamTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class DreamTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var cellView: UIView!
     @IBOutlet weak var titleLabel: AccessibleLabel!
     @IBOutlet weak var dateLabel: AccessibleLabel!
@@ -18,27 +18,21 @@ class DreamTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         // Configure the view for the selected state
     }
     
     var dream: Dream? {
-          didSet {
-              guard let dream = dream else { return }
-              titleLabel.text = dream.title
-              // Handle date below
-//              let date = alarm.date
-//              let dateFormmatter = DateFormatter()
-//              dateFormmatter.dateFormat = "h:mm"
-//              let hour = dateFormmatter.string(from: date)
-//              dateFormmatter.dateFormat = "a"
-//              let symbol = dateFormmatter.string(from: date)
-//              timeLabel.text = hour
-//              amLabel.text = symbol
-//              switchToggle.isOn = alarm.isOn
-          }
-      }
-
+        didSet {
+            guard let dream = dream else { return }
+            titleLabel.text = dream.title
+            let dateFormmatter = DateFormatter()
+            dateFormmatter.dateStyle = .medium
+            dateFormmatter.timeStyle = .short
+            dateLabel.text = (dateFormmatter.string(from: dream.date))
+        }
+    }
+    
 }
