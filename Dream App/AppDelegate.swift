fb012e8f6de0461b7322d509a0d79a3ef84ad131
Perez <perez@sobergrid.com>
2021-02-03 16:49:28 -0500

    changes

--- Dream App/AppDelegate.swift
+++ Dream App/AppDelegate.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Firebase
 import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
+    var docRef : DocumentReference!
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        FirebaseApp.configure()
+        
+        docRef = Firestore.firestore().document("DreamStudio/Dictionary")
+        
+        docRef.getDocument { (documentSnapShot, error) in
+            guard let docuSnapshot = documentSnapShot, documentSnapShot?.exists ?? false else { return }
+            let dictionaryData = docuSnapshot.data()
+            print(dictionaryData)
+        }
         
         UITextField.appearance().keyboardAppearance = UIKeyboardAppearance.dark
         
